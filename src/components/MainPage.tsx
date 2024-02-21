f23fe0d72e860d0cc6439cfb607dc8c79d9824d9
yoji-nakamura <nkmr4ji@gmail.com>
2024-02-21 21:38:52 +0900

    カウンター制作

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,12 @@
+import Title from "./Title";
+import Counter from "./Counter";
+
+function MainPage() {
+  return (
+    <>
+      <Title />
+      <Counter />
+    </>
+  );
+}
+export default MainPage;
